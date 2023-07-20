199f6aa126420e87a4ec741ba8583c68ae4c4282
Joel Lamy-Poirier <joel.lamy-poirier@servicenow.com>
2023-07-20 00:13:15 -0400

    fix_merge

--- flash_attn/flash_attn_interface.py
+++ flash_attn/flash_attn_interface.py
@@ -36,19 +36,9 @@ def _get_block_size(device, head_dim, is_dropout, is_causal):
         return (128, 64) if is_sm80 else (64, 64)
 
 
-<<<<<<< HEAD
 def _flash_attn_forward(q, k, v, dropout_p, softmax_scale, causal, return_softmax, generator=None, seqlen_q=None, seqlen_k=None, softmax_lse=None):
-    if q.stride(-1) != 1:
-        q = q.contiguous()
-    if k.stride(-1) != 1:
-        k = k.contiguous()
-    if v.stride(-1) != 1:
-        v = v.contiguous()
-=======
-def _flash_attn_forward(q, k, v, dropout_p, softmax_scale, causal, return_softmax):
     maybe_contiguous = lambda x: x.contiguous() if x.stride(-1) != 1 else x
     q, k, v = [maybe_contiguous(x) for x in (q, k, v)]
->>>>>>> origin/main
     out, q, k, v, out_padded, softmax_lse, S_dmask = flash_attn_cuda.fwd(
         q, k, v, None, dropout_p, softmax_scale, causal, return_softmax, generator, seqlen_q, seqlen_k, softmax_lse
     )
@@ -56,19 +46,9 @@ def _flash_attn_forward(q, k, v, dropout_p, softmax_scale, causal, return_softma
 
 
 def _flash_attn_varlen_forward(q, k, v, cu_seqlens_q, cu_seqlens_k, max_seqlen_q, max_seqlen_k,
-<<<<<<< HEAD
                                dropout_p, softmax_scale, causal, return_softmax, generator=None, softmax_lse=None):
-    if q.stride(-1) != 1:
-        q = q.contiguous()
-    if k.stride(-1) != 1:
-        k = k.contiguous()
-    if v.stride(-1) != 1:
-        v = v.contiguous()
-=======
-                               dropout_p, softmax_scale, causal, return_softmax):
     maybe_contiguous = lambda x: x.contiguous() if x.stride(-1) != 1 else x
     q, k, v = [maybe_contiguous(x) for x in (q, k, v)]
->>>>>>> origin/main
     out, q, k, v, out_padded, softmax_lse, S_dmask = flash_attn_cuda.varlen_fwd(
         q, k, v, None, cu_seqlens_q, cu_seqlens_k, max_seqlen_q, max_seqlen_k, dropout_p,
         softmax_scale, False, causal, return_softmax, generator, softmax_lse
